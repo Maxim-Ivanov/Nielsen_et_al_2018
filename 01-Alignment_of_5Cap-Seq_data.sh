94868a87b9df93aac73bc4b381e5d67b0963b032
Maxim-Ivanov <35961244+Maxim-Ivanov@users.noreply.github.com>
2018-10-16 13:06:12 +0200

    Add files via upload

--- 01-Alignment_of_5Cap-Seq_data.sh
+++ 01-Alignment_of_5Cap-Seq_data.sh
@@ -1,8 +1,10 @@
-# Quality and adapter trimming (observe the custom adapter sequence):
+# Quality and adapter trimming (observe the custom adapter 
+# sequence):
 for file in *fastq.gz; do echo $file && trim_galore --adapter "ATCTCGTATGCCG" $file; done
 
-# Trim UMIs (first 8 nt of each read), append them to Fastq headers (UMI-Tools required):
-for file in trimmed.fq.gz; do echo $file && umi_tools extract --stdin=${file} --bc-pattern=NNNNNNNN --stdout=${file/.fq.gz/_UMI.fq.gz}; done
+# Trim UMIs (first 8 nt of each read), append them to Fastq 
+# headers (UMI-Tools required):
+for file in *trimmed.fq.gz; do echo $file && umi_tools extract --stdin=${file} --bc-pattern=NNNNNNNN --stdout=${file/.fq.gz/_UMI.fq.gz}; done
 
 # Align to TAIR10 using STAR:
 for file in ./*UMI.fq.gz; do echo $file && STAR --genomeDir /index/tair10/star --readFilesIn $file --runThreadN 4 --outFileNamePrefix ${file/.fq.gz/_} --outSAMmultNmax 1 --alignEndsType EndToEnd --readFilesCommand zcat; done
@@ -19,7 +21,8 @@ for file in *filt.bam; do echo $file && samtools view -h -q 10 $file -o ${file/.
 # Deduplicate on UMIs:
 for file in *mapq10.bam; do echo $file && umi_tools dedup -I $file -S ${file/.bam/_dedup.bam}; done
 
-# Make stranded Bedgraph files (only the first base of each read is considered):
+# Make stranded Bedgraph files (only the first base of each read 
+# is considered):
 for str in "+" "-"; do
   echo $str
   [ "$str" = "+" ] && n="fw" || n="rev"
@@ -31,14 +34,17 @@ done
 # Remove singletons (positions covered by a single tag):
 for file in *bg.gz; do echo $file && zcat $file | awk '{if ($4>1) print $0}' > ${file/.bg.gz/_cov2.bg}; done
 
-# Ensure that all positions have 1 bp width:
+# Ensure that all positions have 1 bp width (this is required for 
+# further processing by CAGEfightR):
 python3 Expand_bedGraph_to_single_base_resolution.py .
 
 # Convert expanded Bedgraph files to Bigwig (requires kentUtils).
 # These Bigwigs will be used as input for CAGEfightR:
 for file in *cov2.bg; do echo $file && bedGraphToBigWig $file TAIR10.chrom.sizes ${file/bg/bw}; done
 
-# Also make Bedgraph files for visualization in genomic browsers. # First, merge forward and reverse Bedgraph files corresponding to the same sample:
+# Also make Bedgraph files for visualization in genomic browsers. # First, merge forward and reverse Bedgraph files corresponding 
+# to the same sample (the strand info is encoded by the sign 
+# of values in column 4):
 for file1 in *fw.bg; do
   file2=${file1/fw/rev}
   outfile=${file1/fw.bg/fw_rev.bg.gz}
