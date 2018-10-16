b56fcc993649d0bb14b380e6b80a664c7734474b
Maxim-Ivanov <35961244+Maxim-Ivanov@users.noreply.github.com>
2018-10-16 13:07:39 +0200

    Delete extractSummits.R

--- extractSummits.R
+++
@@ -1,8 +0,0 @@
-extractSummits <- function(gr) {
-  if ("thick" %in% names(mcols(gr))) {
-    summits <- GRanges(seqnames = seqnames(gr), ranges = mcols(gr)$thick, strand = strand(gr), seqinfo = seqinfo(gr))
-  } else {
-    summits <- GRanges(seqnames = seqnames(gr), ranges = ranges(gr), strand = strand(gr), seqinfo = seqinfo(gr))
-  }
-  summits
-}
